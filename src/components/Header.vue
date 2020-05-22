027f2df2e380232e4b331532f7a8da04bca2e872
napalmpapalam <loktionov.semen.kh@gmail.com>
2020-05-22 14:52:20 +0300

    added vue-material, node-sass, sass-loader and Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <md-toolbar class="md-primary">
+    <h1>DLExchanger</h1>
+    <p>Check ID</p>
+  </md-toolbar>
+</template>
+
+<style lang="scss" scoped>
+.md-toolbar {
+  display: flex;
+  justify-content: space-between;
+}
+
+.md-title {
+  font-weight: 700;
+}
+
+p {
+  font-size: 0.7rem;
+}
+</style>
+<script>
+export default {
+  name: "Header"
+};
+</script>
